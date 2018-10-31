48b3dba863fbbebc77a2a48c6f99d7437ad7d4cc
Nicholas Wilcox <nwilcox@stella.cs.oberlin.edu>
2018-10-31 00:42:49 -0400

    (includes/sidenav.php): Added link to Introductory Problems

--- includes/sidenav.php
+++ includes/sidenav.php
@@ -3,6 +3,8 @@
 
 <ul>
 <?php
+// Creates link to Introductory Problems
+echo "<a href='/problems/show-tag.php?tag=37'><li>Introductory Problems</li></a>";
 /* Creates 18 links on the navbar which the title "Problem Set #" */
 for ($i=1; $i<19; $i++){
   $link = "/problems/show-problem.php?tag=set" . $i;
