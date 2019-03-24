216f16024a832aea8c7ade9aa89fd72968f1f371
Colton Potter <cpotter2@oberlin.edu>
2019-03-24 16:25:37 -0400

    Add(intro/)

---
+++ intro/intro_essay.php
@@ -0,0 +1,37 @@
+<?php
+define("DOCUMENT_ROOT", $_SERVER['DOCUMENT_ROOT']);
+include_once(DOCUMENT_ROOT."/php/constants.php");
+include_once(DOCUMENT_ROOT."/php/functions.php");
+?>
+
+<?php
+$title = "Introductory Essay";
+$pageName = "The Introductory Set of Stella's Stunners";
+include_once(DOCUMENT_ROOT."/includes/header.php");
+?>
+
+
+<?php include_once(DOCUMENT_ROOT."/includes/sidenav.php") ?>
+
+<div id="content">
+
+     <h1><?php echo $pageName ?></h1>
+     <h4>by Rudd Crawford</h4>
+
+     	    <p>Here is a set of twenty problems that, we hope, will show you how the problem library can help you put more emphasis on problem solving with your students.  The basic premise of what my students call "Stellas" is that when your students first face a Stella problem, they don't immediately know what to do. The problems are not an immediate application of what students studied in their textbook today, or even last week.  They are problems that might not require a specific mathematical technique at all.  The point of a Stella problem is that there is no obvious way to get started in finding a solution. Often, there is a certain outrageous quality in the way a the problem taunts and teases them with its difficulty.</p>
+
+	    <p>Thus, the central question about Stellas is, "What do you do when you don't know what to do?"  And a major answer to that question is provided by the list of heuristics that is included with the problem library (see the link on the home page).  Heuristics are simply thinking strategies, suggestions to help you out when you're stuck.  Most of them are pretty simple: "guess an answer and see if it's right--if not, use it to help you guess a better one"; or "draw a picture of the information"; or "break the problem down into smaller bits--determine sub-goals".  These heuristics can be a big help in moving the solver off dead center and starting the mental wheels turning.</p>
+
+	    <p>There's another aspect to the heuristics as tools for thinking.  A difficult problem, especially in mathematics, can be a frightening thing to face.  A typical student reaction is to worry that "there's a formula for doing this but I can't remember it, so I have to give up."  The Stellas, and the heuristics backing them up, can help students keep calm and get going even if they can't remember "the formula." They can be a big help in easing feelings of frustration and encouraging a buoyant involvement with the problem.</p>
+
+	    <p>The problems in this introductory set are discussed in detail, in particular with solutions written out at length, showing the ways in which heuristics from our list can be helpful.  Another part of the discussion covers the ways in which I have used these problems with students.  </p>
+
+	    <p>But all of this discussion is meant to be descriptive, not prescriptive.  All of us in the Stella Project hope that as you begin to explore Stella's Stunners, you will experience the mental charge that comes from figuring out an elusive problem, from "thinking outside of the box" as the common phrase has it these days.  And we hope that you will feel free to experiment with ways of exposing your students to the kind of creative thinking that these problems evoke, and which our education system needs to encourage.</p>
+
+	    <p>I was chatting with a former student of mine, Ian Yarber, big guy, warm, who is now a recreation director for the city of Oberlin and is also on the Oberlin Board of Education.  He said that a youngster came in to see him in his office about a math problem he was stuck on.  He looked startled and asked the youngster, "Haven't you been doing Stella's Stunners?".  The child said he hadn't, and Ian said, "Well, if you'd been doing Stellas you'd have figured out this problem by now."</p>
+
+	    <p>So give these intro problems a whirl, and notice how differently you are using your mind from the way you're doing when you simply study the textbook and do its exercises.  Notice that these problems bring on line the ingenuity and creativity that the American mind is famous for supposedly having.  Notice how you are really and truly thinking, and notice how good it feels to be doing that.  And let us know how it goes.</p>
+
+	    </div>
+
+<?php include(DOCUMENT_ROOT."/includes/footer.php") ?>
