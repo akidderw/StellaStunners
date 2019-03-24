eba4e594f85a6a2ef26328f6da47038ee8c64037
Colton Potter <cpotter2@oberlin.edu>
2019-03-24 16:26:24 -0400

    Add(stella_system/)

---
+++ stella_system/topic.php
@@ -0,0 +1,42 @@
+<?php
+define("DOCUMENT_ROOT", $_SERVER['DOCUMENT_ROOT']);
+include_once(DOCUMENT_ROOT."/php/constants.php");
+include_once(DOCUMENT_ROOT."/php/functions.php");
+include_once DOCUMENT_ROOT."/php/login.php";
+?>
+
+<?php
+$title = "Topics";
+$pageName = "Topics";
+include_once(DOCUMENT_ROOT."/includes/header.php");
+?>
+
+<div id="content">
+     <h1><?php echo $pageName ?></h1>
+
+     <table id="source_table">
+     <?php
+     $file = file("topics.txt");
+     $tableHead = array_shift($file);
+     echo "<thead><tr>";
+     foreach (explode("\t", $tableHead) as $column) {
+	echo "<th>$column</th>";
+	}
+     echo "</tr></thead>";
+     echo "<tbody>";
+     foreach ($file as $line) {
+	echo '<tr>';
+	foreach (explode("\t", $line) as $datum) {
+		echo "<td>$datum</td>";															}
+	echo '</tr>';
+	}
+	echo "</tbody>";
+	?>
+</table>
+
+</div>
+
+<?php include_once(DOCUMENT_ROOT."/includes/sidenav.php") ?>
+
+
+<?php include(DOCUMENT_ROOT."/includes/footer.php") ?>
