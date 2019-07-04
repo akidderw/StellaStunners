20dc616942dc997b18271e9052300a0a55df17dd
Colton Potter <cpotter2@oberlin.edu>
2019-07-03 22:19:59 -0400

    (problems/index.php): Style overhaul

--- problems/index.php
+++ problems/index.php
@@ -1,75 +1,77 @@
 <?php
 define("DOCUMENT_ROOT", $_SERVER['DOCUMENT_ROOT']);
-include_once(DOCUMENT_ROOT."/php/constants.php");
-include_once(DOCUMENT_ROOT."/php/functions.php");
-function setId($ID){
-$problem_id = $ID;
-return $problem_id;
+include_once(DOCUMENT_ROOT . "/php/constants.php");
+include_once(DOCUMENT_ROOT . "/php/functions.php");
+function setId($ID)
+{
+	$problem_id = $ID;
+	return $problem_id;
 }
 ?>
 
 <?php
 $title = "Problems";
 $pageName = "Problems";
-include_once(DOCUMENT_ROOT."/includes/header.php");
-include_once(DOCUMENT_ROOT."/includes/shoppingcartbody.php");
+include_once(DOCUMENT_ROOT . "/includes/header.php");
+include_once(DOCUMENT_ROOT . "/includes/shoppingcartbody.php");
 ?>
 
-<?php include_once(DOCUMENT_ROOT."/includes/sidenav.php") ?>
+<?php include_once(DOCUMENT_ROOT . "/includes/sidenav.php") ?>
 
-<?php require_once(DOCUMENT_ROOT."/php/DBHandler.php") ?>
+<?php require_once(DOCUMENT_ROOT . "/php/DBHandler.php") ?>
 
 <div id="content">
 	<h1><?php echo $pageName ?></h1>
 
 	<p><a href="editor">Problem Editor</a> <a href="checkout.php">Document Creator</a></p>
-	
+
 	<table id="problems_table">
-                <!-- Top Row -->
-                <tr>
-                        <th> Stella Index </th>
-                        <th> Title</th>
+		<!-- Top Row -->
+		<tr>
+			<th> Stella Index </th>
+			<th> Title</th>
 			<th> Add to Cart</th>
-                </tr>
-        
-	        <?php
+		</tr>
+
+		<?php
 		$handle = new Stella();
-	//	if (!isset($GET["tag"]){
-	//		$result = $handle->query('SELECT problems.id, title FROM tagmap, problems WHERE tagmap.tag_id = '$_GET["tag"]' AND tagmap.problem_id = problems.id ORDER BY problem_id');
-	//	}else{
-			$result = $handle->query("SELECT id, title FROM problems ORDER BY id");
-	//	}
+		//	if (!isset($GET["tag"]){
+		//		$result = $handle->query('SELECT problems.id, title FROM tagmap, problems WHERE tagmap.tag_id = '$_GET["tag"]' AND tagmap.problem_id = problems.id ORDER BY problem_id');
+		//	}else{
+		$result = $handle->query("SELECT id, title FROM problems ORDER BY id");
+		//	}
 		while ($row = $result->fetch_assoc()) {
 			echo "<tr><td><a href=\"show-problem.php?id=$row[id]\">$row[id]</td><td>$row[title]</td><td><button class=add-btn type=button id=$row[id] name=$row[id]>Add to Cart</button></td></tr>";
 		}
 		?>
-        </table>
+	</table>
 
 	<script>
-	$(document).ready(function(){
-	// add to cart button listener
-	$('.add-btn').on('click', function(){
-		// info is in the table / single product layout
-		var id = this.name;
-		//var quantity = $(this).find('.cart-quantity').val();
-		// redirect to add_to_cart.php, with parameter values to process the request
-		post(id);
-		//console.log(id);
-		return false;
+		$(document).ready(function() {
+			// add to cart button listener
+			$('.add-btn').on('click', function() {
+				// info is in the table / single product layout
+				var id = this.name;
+				//var quantity = $(this).find('.cart-quantity').val();
+				// redirect to add_to_cart.php, with parameter values to process the request
+				post(id);
+				//console.log(id);
+				return false;
+			});
 		});
-	});
 
-	function post(id){
-		var pid = id;
-		console.log('hello');
-		$.post('add-to-cart.php',{postid:pid},function(data){
-			//$('#content').html(data);
-		});
-		
-	}
+		function post(id) {
+			var pid = id;
+			console.log('hello');
+			$.post('add-to-cart.php', {
+				postid: pid
+			}, function(data) {
+				//$('#content').html(data);
+			});
+
+		}
 	</script>
 	<!-- <p><a href="checkout.php">Make Document</a></p> -->
 
 </div>
-<?php include(DOCUMENT_ROOT."/includes/footer.php") ?>
-
+<?php include(DOCUMENT_ROOT . "/includes/footer.php") ?>
